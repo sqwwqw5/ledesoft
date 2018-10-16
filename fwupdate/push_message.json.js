16ea379b0a9404826c4468ed5a66c2d3839e41a5
fw867 <fw867@163.com>
2018-10-16 11:06:23 +0800

    update message for 2.24

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,9 +1,10 @@
 {
-"title":"<font color='#FF0033'><b>Openwrt X64 V2.23 r8972 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.24 r9105 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>注意：2.13以前版本固件升级到最新版本或者legacy BIOS固件升级到UEFI版固件会清除所有配置。升级前注意保存配置！<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.14.72；<br />\
-<b>2. </b>默认开启full clone,无需安装插件；<br />\
-<b>3. </b>修复bugs；<br />",
+<b>1. </b>升级内核到4.14.75；<br />\
+<b>2. </b>优化full clone支持；<br />\
+<b>3. </b>升级Intel微码、修复N3X系列重启的问题；<br />\
+<b>4. </b>增加一些网卡驱动；<br />",
 }
