ebe0bf06f7539f8d3ecaeba8b7cf364a08046be6
houzi- <wojiaolinmu008@gmail.com>
2019-01-13 23:28:28 +0800

    Fix rules_status

--- koolproxy/koolproxy/scripts/KoolProxy_rules_status.sh
+++ koolproxy/koolproxy/scripts/KoolProxy_rules_status.sh
@@ -18,10 +18,13 @@ pid=`pidof koolproxy`
 #fanboy_nu_local=`grep -E -v "^!" $KSROOT/koolproxy/data/rules/fanboy.txt | wc -l`
 video_rules_local=`cat $KSROOT/koolproxy/data/rules/koolproxy.txt  | sed -n '4p'|awk '{print $3,$4}'`
 
-if [ "$koolproxy_video_rules" == "1" ]; then
-	[ "$koolproxy_video_rules" == "1" ] && http_response "视频规则：$video_rules_local"
+if [ "$status" -ge "1" ]; then
+	if [ "$koolproxy_video_rules" == "1" ]; then
+		[ "$koolproxy_video_rules" == "1" ] && http_response "视频规则：$video_rules_local"
+	else
+		http_response "<font color='#FF0000'>未加载！</font>"
+	fi
 else
 	http_response "<font color='#FF0000'>未加载！</font>"
 fi
-
 
